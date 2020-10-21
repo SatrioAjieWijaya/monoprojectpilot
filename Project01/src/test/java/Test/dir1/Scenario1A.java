2108cafbd45732aec4e2e5c5fae068961c3c2de1
satrio <satrop@alterra.id>
2020-10-21 15:05:52 +0700

    test new CI 02

--- Project01/src/test/java/Test/dir1/Scenario1A.java
+++ Project01/src/test/java/Test/dir1/Scenario1A.java
@@ -11,7 +11,7 @@ public class Scenario1A {
 
     @Given("memberikan fitur SatuA - NolSatu")
     public void memberikan_fitur_SatuA_NolSatu() throws InterruptedException {
-//        System.out.println("memberikan fitur SatuA - NolSatu");
+//        System.out.println("memberikan fitur SatuA - NolSatu");asdasdasd
 //        Thread.sleep(300000);
     }
 
